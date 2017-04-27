079c3f200e83d653d87bb8685bfe86abcd70172b
Juanma <juanmanuel.torres@guadaltech.es>
2017-04-27 15:27:40 +0200

    docker patch

--- statefullset_autoscaler/docker/files/docker-entrypoint.sh
+++ statefullset_autoscaler/docker/files/docker-entrypoint.sh
@@ -3,6 +3,6 @@
 echo "ver 0.0.0.1"
 
 echo "$KMASTER kubernetes" >> /etc/hosts
-echo "$CONF_KUBE" > /slug-containers/credentials/config
+echo "$CONF_KUBE" > /slug-containers/statefullset_autoscaler/slug_statefullset_autoscaler/credentials/config
 
-python /slug-containers/main.py namespace=$NAMESPACE url_heapster=$HEAPSTER autoscaler_count=$AUTOSCALER_COUNT time_query=$TIME_QUERY
+python /slug-containers/statefullset_autoscaler/slug_statefullset_autoscaler/main.py namespace=$NAMESPACE url_heapster=$HEAPSTER autoscaler_count=$AUTOSCALER_COUNT time_query=$TIME_QUERY
