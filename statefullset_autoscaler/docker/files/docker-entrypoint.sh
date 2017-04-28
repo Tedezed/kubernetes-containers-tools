62b0be79a43361b31f772e8c8863bc47c511aed1
Juanma <juanmanuel.torres@guadaltech.es>
2017-04-28 10:19:38 +0200

    Test in Kubernetes

--- statefullset_autoscaler/docker/files/docker-entrypoint.sh
+++ statefullset_autoscaler/docker/files/docker-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo "ver 0.0.0.1"
+echo "ver 0.0.0.3"
 
 echo "$KMASTER kubernetes" >> /etc/hosts
-echo "$CONF_KUBE" > /slug-containers/statefullset_autoscaler/slug_statefullset_autoscaler/credentials/config
+echo $ADD_HOST >> /etc/hosts
+echo -n $CONF_KUBE_BASE64 | base64 -d > /slug-containers/statefullset_autoscaler/slug_statefullset_autoscaler/credentials/config
 
 python /slug-containers/statefullset_autoscaler/slug_statefullset_autoscaler/main.py namespace=$NAMESPACE url_heapster=$HEAPSTER autoscaler_count=$AUTOSCALER_COUNT time_query=$TIME_QUERY
