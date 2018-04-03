05225fd94643f3cdbc489e43d91082a80aef3f35
Juanma <juanmanuel.torres@guadaltech.es>
2018-04-03 18:02:11 +0200

    pgpool ha

---
+++ pgpool_ha/docker/bin/pgpool/failover.sh
@@ -0,0 +1,36 @@
+#! /bin/sh -x
+# Execute command by failover.
+# special values:  %d = node id
+#                  %h = host name
+#                  %p = port number
+#                  %D = database cluster path
+#                  %m = new master node id
+#                  %M = old master node id
+#                  %H = new master node host name
+#                  %P = old primary node id
+#                  %R = new master database cluster path
+#                  %r = new master port number
+#                  %% = '%' character
+
+#pghome=/usr/pgsql-9.6
+#log=/var/log/pgpool/failover.log
+pghome=$PG_HOME
+log=$LOG_FAILOVER
+falling_node=$1          # %d
+old_primary=$2           # %P
+new_primary=$3           # %H
+pgdata=$4                # %R
+
+date >> $log
+echo "failed_node_id=$falling_node new_primary=$new_primary" >> $log
+
+if [ $falling_node = $old_primary ]; then
+    if [ $UID -eq 0 ]
+    then
+        su postgres -c "ssh -T postgres@$new_primary $pghome/bin/pg_ctl promote -D $pgdata"
+    else
+        ssh -T postgres@$new_primary $pghome/bin/pg_ctl promote -D $pgdata
+    fi
+    exit 0;
+fi;
+exit 0;
---
+++ pgpool_ha/docker/bin/pgpool/startpgpool.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# Copyright 2015 Crunchy Data Solutions, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# clean up leftovers from previous runs of pgpool
+rm -rf /tmp/pgpool.pid
+rm -rf /tmp/.s.*
+
+BINDIR=/opt/cpm/bin
+CONFDIR=/opt/cpm/conf/pgpool
+CONFIGS=/tmp
+
+env
+
+function trap_sigterm() {
+	echo "doing trap logic..."
+	kill -SIGINT $PGPOOL_PID
+}
+
+trap 'trap_sigterm' SIGINT SIGTERM
+
+
+# seed with defaults included in the container image, this is the
+# case when /pgconf is not specified
+cp $CONFDIR/* /tmp
+
+# populate template with env vars
+sed -i "s/PG_PRIMARY_SERVICE_NAME/$PG_PRIMARY_SERVICE_NAME/g" $CONFIGS/pgpool.conf
+sed -i "s/PG_REPLICA_SERVICE_NAME/$PG_REPLICA_SERVICE_NAME/g" $CONFIGS/pgpool.conf
+sed -i "s/PG_USERNAME/$PG_USERNAME/g" $CONFIGS/pgpool.conf
+sed -i "s/PG_PASSWORD/$PG_PASSWORD/g" $CONFIGS/pgpool.conf
+
+# Personal configuration
+sed -i "s/PG_NUM_INIT_CHILDREN/$PG_NUM_INIT_CHILDREN/g" $CONFIGS/pgpool.conf
+sed -i "s/PG_MAX_POOL/$PG_MAX_POOL/g" $CONFIGS/pgpool.conf
+sed -i "s/PG_MULTIPLER_BACK/$PG_MULTIPLER_BACK/g" $CONFIGS/pgpool.conf
+sed -i "s/PG_CHILD_LIFE_TIME/$PG_CHILD_LIFE_TIME/g" $CONFIGS/pgpool.conf
+sed -i "s/PG_CLIENT_IDLE_LIMIT/$PG_CLIENT_IDLE_LIMIT/g" $CONFIGS/pgpool.conf
+sed -i "s/PG_MAX_CONNECTIONS/$PG_MAX_CONNECTIONS/g" $CONFIGS/pgpool.conf
+sed -i "s/PG_SUPERUSER_RESERVED_CONNECTIONS/$PG_SUPERUSER_RESERVED_CONNECTIONS/g" $CONFIGS/pgpool.conf
+sed -i "s#PG_DATA_DIRECTORY#$PG_DATA_DIRECTORY#g" $CONFIGS/pgpool.conf
+
+#PCP
+sed -i "s/PCP_PORT/$PCP_PORT/g" $CONFIGS/pgpool.conf
+
+# Debug and log
+sed -i "s/PG_DEBUG/$PG_DEBUG/g" $CONFIGS/pgpool.conf
+sed -i "s/PG_LOG/$PG_LOG/g" $CONFIGS/pgpool.conf
+
+if [ $DOCKER_DEBUG == "on" ]; then
+	sleep 999999999
+fi
+
+# Populate pool_passwd file
+pg_md5 --md5auth --username=$PG_USERNAME --config=$CONFIGS/pgpool.conf $PG_PASSWORD
+pg_md5 --md5auth --username=postgres --config=$CONFIGS/pgpool.conf $POSTGRES_PASSWORD
+
+# PCP without password
+echo "$PG_USERNAME:$(pg_md5 $PG_PASSWORD)" > /etc/pgpool-II-96/pcp.conf
+#export PCPPASSFILE=/tmp/.pcppass
+echo "*:*:$PG_USERNAME:$PG_PASSWORD" > /tmp/.pcppass
+chmod 0600 /tmp/.pcppass
+
+# Start pgpool
+pgpool -n -a $CONFIGS/pool_hba.conf -f $CONFIGS/pgpool.conf  &
+export PGPOOL_PID=$!
+
+# Failover
+sleep 30
+while pgrep -F /tmp/pgpool.pid > /dev/null
+do
+	bash /opt/cpm/bin/failover.sh
+	sleep $TIME_FAILOVER
+done
+
+#pgpool -f /tmp/pgpool.conf  reload
+
+echo "waiting for pgpool to be signaled..."
+wait
