487157a5e46742df84aae464f1adebaf4750cf9f
jmtorres <juanmanuel.torres@guadaltech.es>
2019-02-06 11:07:35 +0100

    Test and debug internal relay smtp

--- secure_emailing/README.md
+++ secure_emailing/README.md
@@ -1,3 +1,36 @@
 # Secure Emailing
 
 Based on: https://github.com/bcoe/secure-smtpd
+
+```
+   -----------                                      --------------------
+  |  Mailjet  | --------------------Port: 25-----> | Destination Server |
+   -----------                                      --------------------
+       ^
+       | Port: 2525
+       |
+       |
+ ----------------- 
+|[Cloud  Firewall]| (Close egress port 25)
+ ----------------- 
+       ^
+       | Port: 2525
+       |
+       |
+ ---------------
+|  Relay email  | // tcpdump -i eth0 'port 25'
+ ---------------
+       ^
+       |  Port: 25
+       |
+ ----------------                             --------------
+|  Secure email  | <---- Port: 5432 -------> |  PostgreSQL  |
+ ----------------                             --------------
+       ^
+       | Port: 9000
+       |
+ ---------------
+|  Client SMTP  |  
+ ---------------
+
+```
--- secure_emailing/docker/common/smtp_proxy_v2/test.py
+++ secure_emailing/docker/common/smtp_proxy_v2/test.py
@@ -1,5 +1,71 @@
+import sys
+
 from credentials_emailing import StoreCredentials
 
-st = StoreCredentials()
+import smtplib, sys, os
+from email.mime.text import MIMEText
 
-print st.validate_mailfrom("foo@demo1.com", "demo" ,"demo")
+def example_execution(num=1):
+	if num == 1:
+		print("""Example: python smtp_proxy_v2/test.py [TEST]
+
+  - validatemail
+  - senmail
+	""")
+	elif num == 2:
+		print("""Example: python smtp_proxy_v2/test.py senmail MAIL_SERVER \
+			MAIL_SERVER_PORT USER PASS YOUR_EMAIL DESTINATION_EMAIL \
+			SUBJECT MESSAGE""")
+	elif num == 3:
+		print("""Example: python smtp_proxy_v2/test.py validatemail \
+			DESTINATION_EMAIL USER PASS""")
+
+if len(sys.argv) > 1:
+	if sys.argv[1] == "validatemail":
+		print("Test: Validatemail using StoreCredentials")
+		st = StoreCredentials()
+		target = sys.argv[2]
+		user = sys.argv[3]
+		passwd = sys.argv[4]
+		print(st.validate_mailfrom(target, user, passwd))
+
+	elif sys.argv[1] == "senmail":
+		print("Test: Send mail to server")
+
+		mailserver = sys.argv[2]
+		mailport = int(sys.argv[3])
+
+		user = sys.argv[4]
+		passwd = sys.argv[5]
+
+		from_email = sys.argv[6]
+		sub = sys.argv[7]
+		targets = [sys.argv[8]]
+		txt = sys.argv[9]
+
+		#targets = os.environ['MAIL_TARGETS'].replace('"', ' ').split(",")
+		targetscorrect = []
+		for mail in targets:
+			date = mail.split()
+			targetscorrect.append(date[0])
+
+		server = smtplib.SMTP(str(mailserver), str(mailport))
+		server.ehlo()
+		#server.starttls()
+		#server.ehlo()
+		server.login(user, passwd)
+
+		msg = MIMEText(txt, "html")
+		msg['Subject'] = sub
+		msg['From'] = from_email
+		msg['To'] = ', '.join(targetscorrect)
+
+		print "txt: %s" % txt
+		if txt != "":
+			for t in targets:
+				print "Send mail to "+t
+				server.sendmail(from_email, t, msg.as_string())
+	else:
+		example_execution()
+else:
+	example_execution()
