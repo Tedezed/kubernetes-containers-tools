6e2fe419b78a2e8f66bfbb4cf425e2573d1c2ef7
jmtorres <juanmanuel.torres@guadaltech.es>
2019-07-15 12:57:17 +0200

    clean logs

--- squirrel/docker/squirrel/main.py
+++ squirrel/docker/squirrel/main.py
@@ -13,6 +13,8 @@ import base64
 from controller.controller import *
 from nuts_manager.nuts_manager import *
 
+gnupg._parsers.Verify.TRUST_LEVELS["ENCRYPTION_COMPLIANCE_MODE"] = 23
+
 class sqrl():
 
     # Basic
